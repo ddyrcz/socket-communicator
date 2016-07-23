0e5f72445f1961f8fc839c877a71f4169a4c19c9
Dawid Dyrcz <dyrcu15@gmail.com>
2016-07-23 16:07:11 +0200

    Fix missing file

---
+++ Client/Views/ClientView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Client.Views
+{
+    /// <summary>
+    /// Interaction logic for ClientView.xaml
+    /// </summary>
+    public partial class ClientView : Window
+    {
+        public ClientView()
+        {
+            InitializeComponent();
+        }
+    }
+}
