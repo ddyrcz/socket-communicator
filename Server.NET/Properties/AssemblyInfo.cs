e9d568bca3fc1cdd0bd9e62f3be02b11cbd5f530
Dawid <dyrcu15@gmail.com>
2016-07-26 13:50:13 +0200

    Rename Server to Server.NET

--- Server.NET/Properties/AssemblyInfo.cs
+++ Server.NET/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetTcpWpfListener")]
+[assembly: AssemblyTitle("Server.NET")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NetTcpWpfListener")]
+[assembly: AssemblyProduct("Server.NET")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
